f6560dce38758f5b2a90318987345c23f7f3a141
Вова <vladimir-shalaev22@yandex.ru>
2017-09-13 14:02:43 +0700

    Небольшое упрощение скрипта

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function slidePosition(start, scroll) {
 
 page.addListener((status) => {
   let scroll = page.scrollTop,
-    vh = $(window).height();
-  slides.eq(0).css('top', slidePosition(0, scroll));
-  slides.eq(1).css('top', slidePosition(vh, scroll));
-  slides.eq(2).css('top', slidePosition(vh * 2, scroll));
+      vh = $(window).height();
+  slides.each(function updatePosition(index, slide) {
+    $(slide).css('top', slidePosition(index * vh, scroll));
+  });
 });
